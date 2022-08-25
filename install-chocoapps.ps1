d7a73184d54846dba7e9b89a5ef6742e873e77b0
ljmitch <ljmitch@gmail.com>
2022-08-25 16:18:32 +0100

    Update install-chocoapps.ps1

--- install-chocoapps.ps1
+++ install-chocoapps.ps1
@@ -1,3 +1,7 @@
+# Setup a new computer with my common apps using chocolatey
+
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/ljmitch/ljmitch/main/install-chocoapps.ps1 -OutFile .\install-chocoapps.ps1; .\install-chocoapps.ps1
+
 $appsUrl = "https://raw.githubusercontent.com/ljmitch/ljmitch/main/apps.txt"
 $appList = Invoke-RestMethod -Uri $appsUrl
 $appListArray = $appList -split "`r`n" | ConvertFrom-Csv
@@ -18,7 +22,7 @@ if ([string]::IsNullOrWhiteSpace($appList) -eq $false -or [string]::IsNullOrWhit
     }
 }
 
-# Install appList
+# Install $appList
 if ([string]::IsNullOrWhiteSpace($appList) -eq $false){   
     Write-Host "Chocolatey Apps Specified"  
     foreach ($app in $appListArray.appList) {
@@ -27,7 +31,7 @@ if ([string]::IsNullOrWhiteSpace($appList) -eq $false){
     }
 }
 
-# Install dismAppList
+# Install $dismAppList
 if ([string]::IsNullOrWhiteSpace($dismAppList) -eq $false){
     Write-Host "DISM Features Specified"    
     foreach ($app in $dismAppListArray.appList) {
